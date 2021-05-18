2faf0a2e2a2639bfde96e1862f7697b62ccaf986
nawangilmi <nawangilmi003@gmail.com>
2021-05-19 01:16:02 +0700

    yihaa

--- menu/hapus_mahasiswa.php
+++ menu/hapus_mahasiswa.php
@@ -2,9 +2,9 @@
 //panggil file koneksi.php yang sudah anda buat
 include "../koneksi.php";
 
-$ID_Mhs=$_GET['ID_Mhs'];   //ambil parameter GET id  dan buat variabel
+$id=$_GET['id'];   //ambil parameter GET id  dan buat variabel
 //gunakan parameter get untuk menghapus data berdasarkan id produk
-$hapus = mysqli_query($koneksi, "DELETE FROM mahasiswa where kode='$ID_Mhs'");
+$hapus = mysqli_query($koneksi, "DELETE FROM mahasiswa where ID_Mhs='$id'");
 if($hapus){ //jika berhasil
     echo "<script>alert('Data Berhasil Di Hapus');document.location='data_mahasiswa.php'</script>";
 }else{  //jika gagal
