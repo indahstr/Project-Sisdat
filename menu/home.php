311407b2bac82e652d0e0975fa850761c46015ef
nawangilmi <nawangilmi003@gmail.com>
2021-05-18 22:24:00 +0700

    begitu

--- menu/home.php
+++ menu/home.php
@@ -132,7 +132,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <li class="header">Menu Utama</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="home.php"><i class='fa fa-home'></i> <span> Dashboard</span></a></li>
-            <li><a href="#"><i class='fa fa-institution'></i> <span> Mahasiswa</span><i class="fa fa-angle-left pull-right"></i></a> 
+            <li><a href="#"><i class='fa fa-address-book'></i> <span> Mahasiswa</span><i class="fa fa-angle-left pull-right"></i></a> 
                 <ul class="treeview-menu">
                 <li class="active"><a href="data_mahasiswa.php"><i class="fa fa-mail-forward"></i> Mahasiswa</a></li>
                 <li><a href="tambah_mahasiswa.php"><i class="fa fa-plus-square-o"></i> Tambah Data Mahasiswa</a></li>
